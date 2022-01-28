f3864e25f243480370bc575619445bd74f4a2ece
priyanka <priyankapendela111@gmail.com>
2022-01-28 17:16:21 +0530

    [Rasi]Added the implementation

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -7,21 +7,36 @@ namespace LinkedList
     internal class LinkedList
     {
         public Node head;
-             //---------------------------- creating method for a class----------------------------//
-        public void AddFront(int new_data)
+        ///---------------- creating method for a class--------------------///
+        public void AddAppend(int new_data)
         {
-            //-------------- Creating a object for class-------------------------------------------//
+            ///---- Creating a object for class---------------------------///
             Node new_node = new Node(new_data);
-
-            //----- when we want to add any node at the front , we want the head to point to it----//
-            new_node.next=this.head;
-
-            //----- the previous head node is now the second node of linked list-------------------//
-            this.head=new_node;
-
-            Console.WriteLine("inserted into list" +" " + new_node.data);
+            if (this.head == null)
+            {
+                this.head = new_node;
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = new_node;
+            }
+            Console.WriteLine("inserted into list" + " " + new_node.data);
         }
-            //-------Display Method is used to display Elements-----------//
+        ///-------Get last node reference-------------------------------///
+        ///-------Method Starts------------------///
+        public Node GetLastNode()
+        {
+            Node temp = this.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        ////--------Method Ends------------------///
+        
+        ///--------------Displaying the elements------------------------///
         public void Display()
         {
             Console.WriteLine("Displaying Names");
